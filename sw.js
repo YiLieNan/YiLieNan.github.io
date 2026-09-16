a81b468ee179a8f13f787c4eedc1c9387261f382
YiLieNan <yilienan@users.noreply.github.com>
2026-09-16 08:10:47 +0800

    Site updated: 2026-09-16 08:10:37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1789478967838";const preCache = ["/images/taichi.png","/images/elaina-banner-desktop.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789517425825";const preCache = ["/images/taichi.png","/images/elaina-banner-desktop.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
